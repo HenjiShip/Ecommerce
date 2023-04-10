cb964fa317d014ee4a3372d6a9efc2aae9c9a5d4
Henry <henry.li@sesame.org>
2023-04-10 17:26:12 -0400

    changed base url to the proper drizco url for deployment

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8888/.netlify/functions/api",
+  baseURL: "https://drizco.netlify.app/.netlify/functions/api",
   headers: {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
   },
