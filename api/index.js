8bc84d1d6c5c7112524f77e5f13197a69873167b
Henry <henry.li@sesame.org>
2023-04-12 14:39:49 -0400

    changed base url back cause it wasn't working on netlify

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_NETLIFY_API,
+  baseURL: "https://drizco.netlify.app/.netlify/functions/api",
   headers: {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
   },
